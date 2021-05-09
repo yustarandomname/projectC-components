bfa23f57e01b4ea338f837370889f59acbbcf1e7
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-05-09 22:25:33 +0200

    loading view

---
+++ src/Loading.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let height = "1em"
+</script>
+
+<style>
+  .loading {
+    width:100%;
+    position: relative;
+  }
+
+  .loadingAnimation {
+    position:absolute;
+    left:0%;
+    height:100%;
+    width:40%;
+    background: rgb(219, 219, 219);
+    background: linear-gradient(135deg, rgba(255,255,255,0) 10%, rgba(228,228,228,0.5) 20%, rgba(255,255,255,0) 100%);
+    animation: loading 5s infinite;
+  }
+
+  @keyframes loading {
+    from {left:0%}
+    to {left:40%}
+  }
+</style>
+
+<div class="loading" style="height:{height}">
+  <div class="loadingAnimation"></div>
+</div>
